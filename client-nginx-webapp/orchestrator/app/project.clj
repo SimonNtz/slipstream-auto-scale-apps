59f7f4013f65f1367894d19e9d6cee019c59960a
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2016-04-07 15:52:30 +0200

    initial commit with client-nginx-webapp application.

---
+++ client-nginx-webapp/orchestrator/app/project.clj
@@ -0,0 +1,10 @@
+(defproject streams "2.24-SNAPSHOT"
+            :description "Clojure SlipStream Client"
+            :url "http://sixsq.com"
+
+            :license {:name "Apache License, Version 2.0"
+                      :url  "http://www.apache.org/licenses/LICENSE-2.0"}
+            :dependencies [[org.clojure/clojure "1.8.0"]
+                           [clj-http "2.0.0"]
+                           [cheshire "5.5.0"]
+                           [riemann "0.2.10"]])
