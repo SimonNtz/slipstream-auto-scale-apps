7069ac1a613ef0875960535f6f94fac53b97aa2b
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2016-06-23 12:47:43 +0200

    use UDP instead of TCP to avoid metrics batching

--- client-nginx-webapp/webapp/deployment/collectd.sh
+++ client-nginx-webapp/webapp/deployment/collectd.sh
@@ -61,7 +61,7 @@ LoadPlugin write_riemann
     <Node "local">
         Host "$riemann_host"
         Port "$riemann_port"
-        Protocol TCP
+        Protocol UDP
         StoreRates true
         AlwaysAppendDS false
     </Node>
