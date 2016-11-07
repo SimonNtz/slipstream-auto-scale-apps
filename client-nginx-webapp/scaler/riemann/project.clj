7f5fd17a3e3baa02fea175d5e1e47e27cd0e9963
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2016-11-07 10:42:07 +0100

    initial refactor extracting SS scale related bits to a separate script.

--- client-nginx-webapp/scaler/riemann/project.clj
+++ client-nginx-webapp/scaler/riemann/project.clj
@@ -7,8 +7,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.0.0"]
                  [cheshire "5.5.0"]
-                 [riemann "0.2.11"]
-                 [com.sixsq.slipstream/SlipStreamClientAPI-jar "3.10"]]
+                 [riemann "0.2.11"]]
   :repositories [["sixsq" {:url      "http://nexus.sixsq.com/content/repositories/releases-community-rhel7"
                            :username "pass"
                            :password :env}]])
